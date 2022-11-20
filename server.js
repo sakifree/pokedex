108d90621c1ff9ddc3756f0a62154d745e1dffb9
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-19 23:43:50 -0500

    server works

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /********************************* */
 // DECLARE DEPENDENCIES
 /********************************* */
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
