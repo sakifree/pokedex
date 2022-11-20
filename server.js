fbe6cc6c69ff3b9da6fd7f482519533d1066b8d7
Sakinah Freeman <freemansakinah@gmail.com>
2022-11-19 23:41:14 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+/********************************* */
+// DECLARE DEPENDENCIES
+/********************************* */
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const pokemon = require("./models/pokemon")
+
+/********************************* */
+// MIDDLEWARE
+/********************************* */
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended:true }))
+app.use(express.static("public"))
+
+/********************************* */
+// ROUTES
+/********************************* */
+app.get("/", (req, res) => {
+    res.send("This server is doing what it needs to do!")
+})
+
+/********************************* */
+// SERVER LISTENER
+/********************************* */
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Nothing feels better than feeling the love on port: ${PORT}`)
+})
